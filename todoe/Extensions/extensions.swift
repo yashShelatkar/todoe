3f213a6a3eed069022e5a421ed2e41dc2ab4e58c
Yash <shelatkaryash@gmail.com>
2019-07-05 11:29:11 +1000

    created Extension for CG Colors

---
+++ todoe/Extensions/extensions.swift
@@ -0,0 +1,18 @@
+//
+//  extensions.swift
+//  todoe
+//
+//  Created by yash Shelatkar on 5/7/19.
+//  Copyright © 2019 yash Shelatkar. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        
+        self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1.0)
+        
+       
+    }
+}
--- todoe/TOGradient.swift
+++ todoe/TOGradient.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class TOGradient:UIView {
     
+    
+    var colors:[CGColor] = [
+        UIColor.init(red: 100, green: 228, blue: 255).cgColor,
+        UIColor.init(red: 58, green: 123, blue: 213).cgColor
+    ]
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.translatesAutoresizingMaskIntoConstraints = false
         if let layer = self.layer as? CAGradientLayer {
-            layer.colors = [UIColor.blue.cgColor, UIColor.gray.cgColor]
+            layer.colors = self.colors
             layer.locations = [0.0, 1.2]
         }
         
