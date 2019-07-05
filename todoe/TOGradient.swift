1b30c45655d835afe96153545f8d4b1d06d06782
Yash <shelatkaryash@gmail.com>
2019-07-05 11:39:05 +1000

    created Extension for Custom Hex COLOR

--- todoe/TOGradient.swift
+++ todoe/TOGradient.swift
@@ -17,12 +17,21 @@ class TOGradient:UIView {
     ]
     
     
+    var hexColors:[CGColor] = [
+        UIColor.init(rgb:0x3A7BD5).cgColor,
+        UIColor.init(rgb:0x64E4FF).cgColor
+    ]
+ 
+    // Color COdes HEX
+    // 3A7BD5
+    // 64E4FF
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.translatesAutoresizingMaskIntoConstraints = false
         if let layer = self.layer as? CAGradientLayer {
-            layer.colors = self.colors
+            layer.colors = self.hexColors
             layer.locations = [0.0, 1.2]
         }
         
