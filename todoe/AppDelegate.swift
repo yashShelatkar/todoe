dcb8dc4b50c46d545b69aef6311ed45e2b308d76
Yash <shelatkaryash@gmail.com>
2019-07-05 10:43:18 +1000

    Removed Story bord

--- todoe/AppDelegate.swift
+++ todoe/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let welcomeScreen = ViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = welcomeScreen
         return true
     }
 
