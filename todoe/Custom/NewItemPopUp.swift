0a6ed17bc2e30f86346f306c282b4acabf9aede8
Yash <shelatkaryash@gmail.com>
2019-07-16 10:55:12 +1000

    added button selectors

--- todoe/Custom/NewItemPopUp.swift
+++ todoe/Custom/NewItemPopUp.swift
@@ -13,6 +13,14 @@ class NewItemPopUp:TOGradient {
     let cancel = TOButton(title: "  cancel  ", type: .roundedText, radius: 4)
     let add = TOButton(title: "  add  ", type: .roundedText, radius: 4)
     
+    @objc func handelCancel(){
+        print("try to handel cancel")
+    }
+    
+    @objc func handelAdd(){
+        print("try to handel add")
+    }
+    
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
@@ -31,7 +39,15 @@ class NewItemPopUp:TOGradient {
         add.rightAnchor.constraint(equalTo: rightAnchor,constant: insert - 32).isActive = true
         add.topAnchor.constraint(equalTo: topAnchor, constant:  insert).isActive = true
         add.heightAnchor.constraint(equalToConstant: 20).isActive = true
+    
+        
+        cancel.addTarget(self, action: #selector(self.handelCancel), for: .touchUpInside)
+        add.addTarget(self, action: #selector(self.handelAdd), for: .touchUpInside)
+        
     }
+    
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
