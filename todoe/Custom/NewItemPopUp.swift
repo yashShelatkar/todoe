3e935daeb0d8898b08ed9f999d1687b3f0074033
Yash <shelatkaryash@gmail.com>
2019-07-16 10:48:09 +1000

    add popup Button added

--- todoe/Custom/NewItemPopUp.swift
+++ todoe/Custom/NewItemPopUp.swift
@@ -11,11 +11,13 @@ import UIKit
 class NewItemPopUp:TOGradient {
     
     let cancel = TOButton(title: "  cancel  ", type: .roundedText, radius: 4)
+    let add = TOButton(title: "  add  ", type: .roundedText, radius: 4)
     
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
         let insert:CGFloat = 16
+        
         
         self.layer.cornerRadius = 14
         
@@ -24,6 +26,11 @@ class NewItemPopUp:TOGradient {
         cancel.leftAnchor.constraint(equalTo: leftAnchor,constant: insert).isActive = true
         cancel.topAnchor.constraint(equalTo: topAnchor, constant:  insert).isActive = true
         cancel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        addSubview(add)
+        add.rightAnchor.constraint(equalTo: rightAnchor,constant: insert - 32).isActive = true
+        add.topAnchor.constraint(equalTo: topAnchor, constant:  insert).isActive = true
+        add.heightAnchor.constraint(equalToConstant: 20).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
